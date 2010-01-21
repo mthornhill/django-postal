66473cd8ee876e904ad31fb2071039969f1fcf64
Michael Thornhill <michael.thornhill@gmail.com>
2010-01-21 14:23:54 +0000

    initial django-postal app structure from http://jacobian.org/writing/django-apps-with-buildout/

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-postal",
+    version = "0.1",
+    url = 'http://github.com/mthornhill/django-postal',
+    license = 'BSD',
+    description = "A Django app for l10n of postal addresses.",
+    long_description = read('README'),
+
+    author = 'Michael Thornhill',
+    author_email = 'michael@maithu.com',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+    install_requires = ['setuptools'],
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
---
+++ src/postal/testsettings.py
@@ -0,0 +1,4 @@
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = '/tmp/postal.db'
+INSTALLED_APPS = ['postal']
+ROOT_URLCONF = ['postal.urls']
