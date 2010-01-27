616f8f85f2d109b93d0554d298f435443e3e78ed
Michael Thornhill <michael.thornhill@gmail.com>
2010-01-27 12:24:49 +0000

    modify library to point at forms modules

--- src/postal/library.py
+++ src/postal/library.py
@@ -1,7 +1,12 @@
-import ie, de
+from ie.forms import IEPostalAddressForm
+from de.forms import DEPostalAddressForm
+from gb.forms import GBPostalAddressForm
+from us.forms import USPostalAddressForm
 
 
-country_map = {"de": de.DEPostalAddressForm, 
-               "ie": ie.IEPostalAddressForm,
+country_map = {"de": DEPostalAddressForm, 
+               "ie": IEPostalAddressForm,
+               "gb": GBPostalAddressForm,
+               "us": USPostalAddressForm,
               }
 
