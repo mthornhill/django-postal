0ab37815366144ae4a313c25e1d5975c86a66d98
Michael Thornhill <michael.thornhill@gmail.com>
2010-01-31 19:42:44 +0000

    enable admin functionality for testing

--- src/postal/tests/urls.py
+++ src/postal/tests/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
+from django.contrib import admin
+admin.autodiscover()
+
+import os
+DIRNAME = os.path.dirname(__file__)
 
 urlpatterns = patterns("",
-    (r'', 'postal.tests.views.test_postal'),
+    (r'^$', 'postal.tests.views.test_postal'),
+)
+
+urlpatterns += patterns("",
+    (r'^admin/(.*)', admin.site.root),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(DIRNAME, "media"), 'show_indexes': True }),
 )
--- src/postal/testsettings.py
+++ src/postal/testsettings.py
@@ -1,6 +1,10 @@
 DEBUG=True
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = '/tmp/postal.db'
-INSTALLED_APPS = ['countries',
+INSTALLED_APPS = ['django.contrib.admin',
+                  'django.contrib.auth',
+                  'django.contrib.contenttypes',
+                  'django.contrib.sessions',
+                  'countries',
                   'postal',]
 ROOT_URLCONF = 'postal.tests.urls'
