7811a28d10080fa0cf26fd98bde42cb9f79474a0
Michael Thornhill <michael.thornhill@gmail.com>
2010-01-31 16:09:40 +0000

    compare lower case of country code

--- src/postal/views.py
+++ src/postal/views.py
@@ -5,6 +5,6 @@ from postal.forms import PostalAddressForm
 from postal.library import country_map
 
 def get_postal_form_class(country_code):
-    return country_map.get(country_code, PostalAddressForm)
+    return country_map.get(country_code.lower(), PostalAddressForm)
 
 
