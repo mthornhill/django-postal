edf543ba3dfbf05105e948db1f6fe466207c761f
Michael Thornhill <michael.thornhill@gmail.com>
2010-02-02 21:22:37 +0000

    allow the default address to be modified in settings

--- src/postal/forms.py
+++ src/postal/forms.py
@@ -2,13 +2,14 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from models import PostalAddress
+from settings import POSTAL_ADDRESS_LINE1, POSTAL_ADDRESS_LINE2, POSTAL_ADDRESS_LINE3, POSTAL_ADDRESS_LINE4, POSTAL_ADDRESS_LINE5
 
 class PostalAddressForm(forms.ModelForm):
-    line1 = forms.CharField(label=_(u"Company name"), required=False, max_length=100)
-    line2 = forms.CharField(label=_(u"Street"), required=False, max_length=100)
-    line3 = forms.CharField(label=_(u"City"), required=False, max_length=100)
-    line4 = forms.CharField(label=_(u"State"), required=False, max_length=100)
-    line5 = forms.CharField(label=_(u"Zip Code"), required=False, max_length=100)
+    line1 = forms.CharField(label=POSTAL_ADDRESS_LINE1[0], required=POSTAL_ADDRESS_LINE1[1], max_length=100)
+    line2 = forms.CharField(label=POSTAL_ADDRESS_LINE2[0], required=POSTAL_ADDRESS_LINE2[1], max_length=100)
+    line3 = forms.CharField(label=POSTAL_ADDRESS_LINE3[0], required=POSTAL_ADDRESS_LINE3[1], max_length=100)
+    line4 = forms.CharField(label=POSTAL_ADDRESS_LINE4[0], required=POSTAL_ADDRESS_LINE4[1], max_length=100)
+    line5 = forms.CharField(label=POSTAL_ADDRESS_LINE5[0], required=POSTAL_ADDRESS_LINE5[1], max_length=100)
 
     class Meta:
         model = PostalAddress
