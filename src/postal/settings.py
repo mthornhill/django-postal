be8ea286549a64a8d6ab1b3bc7009634495338f4
Michael Thornhill <michael.thornhill@gmail.com>
2010-02-03 09:59:44 +0000

    new settings file for default address labels and required

---
+++ src/postal/settings.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+# each address line is a tuple of format (field_label, required)
+POSTAL_ADDRESS_LINE1 = getattr(settings, "POSTAL_ADDRESS_LINE1", (_(u"Company name"), False))
+POSTAL_ADDRESS_LINE2 = getattr(settings, "POSTAL_ADDRESS_LINE2", (_(u"Street"), False))
+POSTAL_ADDRESS_LINE3 = getattr(settings, "POSTAL_ADDRESS_LINE3", (_(u"City"), False))
+POSTAL_ADDRESS_LINE4 = getattr(settings, "POSTAL_ADDRESS_LINE4", (_(u"State"), False))
+POSTAL_ADDRESS_LINE5 = getattr(settings, "POSTAL_ADDRESS_LINE5", (_(u"Zip code"), False))
