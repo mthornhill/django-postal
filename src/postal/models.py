5e8dd50b01785002dbaaa4faf721a1c478614d06
Michael Thornhill <michael.thornhill@gmail.com>
2010-02-03 16:59:00 +0000

    supporting pysqlite2 buildout for geo aware models

--- src/postal/models.py
+++ src/postal/models.py
@@ -5,12 +5,11 @@ http://www.djangosnippets.org/snippets/912/ in the future
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-#from django.contrib.gis.db import models as gis_models
+from django.contrib.gis.db import models as gis_models
 
 from countries.models import Country
 
-#class PostalAddress(gis_models.Model):
-class PostalAddress(models.Model):
+class PostalAddress(gis_models.Model):
     firstname = models.CharField(_("Firstname"), max_length=100, blank=True, null=True)
     lastname = models.CharField(_("Lastname"), max_length=100, blank=True, null=True)
     line1 = models.CharField(_("Line 1"), max_length=100, blank=True, null=True)
@@ -19,8 +18,8 @@ class PostalAddress(models.Model):
     line4 = models.CharField(_("Line 4"), max_length=100, blank=True, null=True)
     line5 = models.CharField(_("Line 5"), max_length=100, blank=True, null=True)
     country = models.ForeignKey(Country, verbose_name=_("Country"), blank=True, null=True)
-    #location = gis_models.PointField(_("Location"), blank=True, null=True)    
-    #objects = gis_models.GeoManager()
+    location = gis_models.PointField(_("Location"), blank=True, null=True)
+    objects = gis_models.GeoManager()
 
     def __unicode__(self):
         return "%s %s, %s" % (self.firstname, self.lastname, self.country)
