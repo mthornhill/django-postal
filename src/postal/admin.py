1f7436518830ebf07b409e4cb2a020e30968be03
Michael Thornhill <michael.thornhill@gmail.com>
2010-06-21 11:58:14 +0100

    add some information to the django admin interface

--- src/postal/admin.py
+++ src/postal/admin.py
@@ -1,9 +1,9 @@
 # django imports
-#from django.contrib.gis import admin
 from django.contrib import admin
-
-# lfs imports
 from postal.models import PostalAddress
 
-admin.site.register(PostalAddress)
-#admin.site.register(PostalAddress,admin.GeoModelAdmin)
+class PostalAddressAdmin(admin.ModelAdmin):
+    list_display = ['line1', 'line2', 'city', 'state', 'code', 'country']
+    list_filter = ['country', 'state']
+
+admin.site.register(PostalAddress, PostalAddressAdmin)
