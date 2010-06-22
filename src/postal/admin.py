12fda23439424d903ffdd6ffb37827e652c7ac90
Michael Thornhill <michael.thornhill@gmail.com>
2010-06-22 14:10:00 +0100

    change order of filters

--- src/postal/admin.py
+++ src/postal/admin.py
@@ -4,6 +4,6 @@ from postal.models import PostalAddress
 
 class PostalAddressAdmin(admin.ModelAdmin):
     list_display = ['line1', 'line2', 'city', 'state', 'code', 'country']
-    list_filter = ['country', 'state']
+    list_filter = ['state', 'country']
 
 admin.site.register(PostalAddress, PostalAddressAdmin)
