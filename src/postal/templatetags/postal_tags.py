2808e7415195db23e2ae71eb179bf3e7dc06d991
Michael Thornhill <michael.thornhill@gmail.com>
2010-09-09 18:23:30 +0100

    save post data

--- src/postal/templatetags/postal_tags.py
+++ src/postal/templatetags/postal_tags.py
@@ -11,5 +11,8 @@ register = template.Library()
 @register.inclusion_tag('postal/address_inline.html')
 def show_postal_address(request, prefix="", country_code=None, template_name="postal/address_inline.html"):
     address_form_class = get_postal_form_class(country_code)
-    address_form = address_form_class(prefix=prefix)
+    if request.method == "POST":
+        address_form = address_form_class(prefix=prefix, data=request.POST)
+    else:
+        address_form = address_form_class(prefix=prefix)
     return {'address_form': address_form, 'MEDIA_URL': settings.MEDIA_URL, 'prefix': prefix}
