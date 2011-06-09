a41058906602a5e5cb19afa1a3b411ab16ff2bc7
Michael Thornhill <michael.thornhill@gmail.com>
2011-06-09 19:47:20 +0200

    v0.7.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "django-postal",
-    version = "0.7.6",
+    version = "0.7.7",
     url = 'http://github.com/mthornhill/django-postal',
     license = 'BSD',
     description = "A Django app for l10n of postal addresses.",
