e3a5dcecc7d5e103e7e56356ee9139b24a193589
Michael Thornhill <michael.thornhill@gmail.com>
2012-09-11 07:26:58 +0100

    version 0.93 with polish form fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "django-postal",
-    version = "0.92",
+    version = "0.93",
     url = 'http://github.com/mthornhill/django-postal',
     license = 'BSD',
     description = "A Django app for l10n of postal addresses.",
