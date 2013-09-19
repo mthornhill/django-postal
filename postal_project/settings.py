c0799fcb9ec319ed5efd6b062a8e77f11c0ee6e3
mick <michael@maithu.com>
2013-09-19 19:33:25 +0100

    update buildout and settings.py for later version of django

--- postal_project/settings.py
+++ postal_project/settings.py
@@ -1,11 +1,22 @@
 import os
+
 DIRNAME = os.path.dirname(__file__)
 
-DEBUG=True
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '/tmp/postal.db'
+DEBUG = True
 
-SITE_ID=1
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/tmp/postal.db', # Or path to database file if using sqlite3.
+        'USER': '', # Not used with sqlite3.
+        'PASSWORD': '', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+SITE_ID = 1
 
 MEDIA_ROOT = DIRNAME + "/media"
 MEDIA_URL = '/site_media/'
@@ -15,11 +26,12 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = ['django.contrib.admin',
-                  'django.contrib.auth',                  
+                  'django.contrib.auth',
                   'django.contrib.contenttypes',
                   'django.contrib.sessions',
                   'django.contrib.sites',
                   'django_countries',
-                  'postal',]
+                  'postal', ]
 ROOT_URLCONF = 'postal_project.urls'
 
+SECRET_KEY = "abc123"
