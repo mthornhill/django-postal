411de4166391ae42c71057f60817b8dc6915bad9
tzicatl <nnieto@noenieto.com>
2013-10-31 18:38:17 -0700

    Support for Mexican states

--- src/postal/library.py
+++ src/postal/library.py
@@ -1,14 +1,15 @@
 from django import forms
 from postal import settings as postal_settings
 from postal.forms import PostalAddressForm
+from postal.forms.cz.forms import CZPostalAddressForm
 from postal.forms.de.forms import DEPostalAddressForm
 from postal.forms.gb.forms import GBPostalAddressForm
 from postal.forms.ie.forms import IEPostalAddressForm
+from postal.forms.mx.forms import MXPostalAddressForm
 from postal.forms.nl.forms import NLPostalAddressForm
-from postal.forms.us.forms import USPostalAddressForm
-from postal.forms.cz.forms import CZPostalAddressForm
 from postal.forms.pl.forms import PLPostalAddressForm
 from postal.forms.ru.forms import RUPostalAddressForm
+from postal.forms.us.forms import USPostalAddressForm
 
 # TODO: Auto-import these forms
 country_map = {
@@ -16,6 +17,7 @@ country_map = {
     "de": DEPostalAddressForm,
     "gb": GBPostalAddressForm,
     "ie": IEPostalAddressForm,
+    "mx": MXPostalAddressForm,
     "nl": NLPostalAddressForm,
     "pl": PLPostalAddressForm,
     "ru": RUPostalAddressForm,
