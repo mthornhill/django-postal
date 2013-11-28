371fcd712c73162e86386940dcab81cbabe40cb3
tzicatl <nnieto@noenieto.com>
2013-11-27 21:57:15 -0800

    fix postal_poject for Django 1.5

--- postal_project/urls.py
+++ postal_project/urls.py
@@ -7,10 +7,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns("",
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', include(admin.site.urls)),
     (r'^postal/', include('postal.urls')),
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.DIRNAME, "media"), 'show_indexes': True }),
     url(r'^$', 'postal_project.views.test_postal', name="postal-home"),
     url(r'^json$', 'postal_project.views.test_postal_json', name="postal-home"),
-    
+
 )
