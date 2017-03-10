27a83397ed1639fc517b420fe83a732df33fc8eb
Kai Diefenbach <kai.diefenbach@iqpp.de>
2017-03-10 08:18:11 +0100

    Add forms for AT and CH

---
+++ src/postal/forms/ch/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from localflavor.ch.forms import CHZipCodeField
+
+from postal.forms import PostalAddressForm
+
+
+class CHPostalAddressForm(PostalAddressForm):
+    line1 = forms.CharField(label=_(u"Street"), max_length=100)
+    city = forms.CharField(label=_(u"City"), max_length=100)
+    code = CHZipCodeField(label=_(u"Zip Code"))
+
+    def __init__(self, *args, **kwargs):
+        super(CHPostalAddressForm, self).__init__(*args, **kwargs)
+        self.fields.pop('line2')
+        self.fields.pop('state')
+        self.fields['country'].initial = "CH"
+        self.fields.keyOrder = ['line1', 'code', 'city', 'country']
